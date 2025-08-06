ebe2d9a02624c7b1df0820d9667432eb4c9c5f6b
Emir Teófilo Calife Neto <emircalife@gmail.com>
2025-08-06 12:59:43 -0300

    Ajustes da Parte 01
    Ajustes da Parte 01

--- Services/RandomService.cs
+++ Services/RandomService.cs
@@ -6,24 +6,28 @@ namespace ProvaPub.Services
 {
 	public class RandomService
 	{
-		int seed;
-        TestDbContext _ctx;
-		public RandomService()
+        private static readonly Random _rng = new Random();
+        private readonly TestDbContext _ctx;
+
+        public RandomService()
         {
             var contextOptions = new DbContextOptionsBuilder<TestDbContext>()
-    .UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=Teste;Trusted_Connection=True;")
-    .Options;
-            seed = Guid.NewGuid().GetHashCode();
-
+                                   .UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=Teste;Trusted_Connection=True;")
+                                   .Options;
             _ctx = new TestDbContext(contextOptions);
         }
-        public async Task<int> GetRandom()
-		{
-            var number =  new Random(seed).Next(100);
-            _ctx.Numbers.Add(new RandomNumber() { Number = number });
-            _ctx.SaveChanges();
-			return number;
-		}
 
+        public async Task<int> GetRandom()
+        {
+            int number;
+            do
+            {
+                number = _rng.Next(100);  // ou Random.Shared.Next(100)
+            } while (_ctx.Numbers.Any(n => n.Number == number));
+
+            _ctx.Numbers.Add(new RandomNumber { Number = number });
+            await _ctx.SaveChangesAsync();
+            return number;
+        }
 	}
 }
